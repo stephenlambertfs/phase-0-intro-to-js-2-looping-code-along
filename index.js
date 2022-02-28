4a69612bd49dd44c9b7130e42dd98e5dcd2c4be1
stephenlambertfs <97977357+stephenlambertfs@users.noreply.github.com>
2022-02-28 12:17:07 -0700

    commit tree ee06b759f1d7915265844d84df3df15530dd5335

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    let thankYouCards = [];
+    for (let i = 0; i < names.length; i++) {
+      thankYouCards.push(
+        `Thank you, ${names[i]} , for the wonderful ${event} gift!`
+      );
+    }
+    return thankYouCards;
+  }
